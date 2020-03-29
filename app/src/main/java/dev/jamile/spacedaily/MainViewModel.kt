da745adf2fa29d090db0e73e4ddb272d182417f1
Jamile <jamilelima@protonmail.com>
2020-03-29 15:41:05 -0300

    chore: improve MainViewModel

--- app/src/main/java/dev/jamile/spacedaily/MainViewModel.kt
+++ app/src/main/java/dev/jamile/spacedaily/MainViewModel.kt
@@ -2,22 +2,27 @@ package dev.jamile.spacedaily
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dev.jamile.spacedaily.network.NasaApiInterface
 import dev.jamile.spacedaily.network.PhotoResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
 interface MainView {
     fun setDailyPhoto(dailyPhoto: PhotoResponse)
 }
 
-class MainViewModel : ViewModel() {
+class MainViewModel : ViewModel(), KoinComponent {
+    val nasaApiInterface: NasaApiInterface by inject()
     var view: MainView? = null
 
     fun getDailyPhoto() {
         viewModelScope.launch(Dispatchers.IO) {
+            val dailyPhoto = nasaApiInterface.getDailyPhoto()
             withContext(Dispatchers.Main) {
-
+                view?.setDailyPhoto(dailyPhoto)
             }
         }
     }
