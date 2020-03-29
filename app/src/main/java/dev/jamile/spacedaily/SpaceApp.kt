19acd9aa5307b5848022d26fabeff0f91e8c9c98
Jamile <jamilelima@protonmail.com>
2020-03-29 15:41:30 -0300

    chore: create SpaceApp

--- app/src/main/java/dev/jamile/spacedaily/SpaceApp.kt
+++ app/src/main/java/dev/jamile/spacedaily/SpaceApp.kt
@@ -1,2 +1,23 @@
 package dev.jamile.spacedaily
 
+import android.app.Application
+import dev.jamile.spacedaily.di.appModule
+import dev.jamile.spacedaily.di.networkModule
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class SpaceApp : Application() {
+    val defaultCurrentActivityListener: DefaultCurrentActivityListener by inject()
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@SpaceApp)
+            modules(listOf(networkModule, appModule))
+        }
+        registerActivityLifecycleCallbacks(defaultCurrentActivityListener)
+    }
+}
